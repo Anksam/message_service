6dbde6dd274533534e05732ce4af8da0b678ae47
Ankesh <aankeshsamarth@gmail.com>
2017-03-02 03:37:14 +0530

    Ensured Data Integrity with ActiveRecord Validations

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe Notification, type: :model do
     end
   end
 
+  xit 'requires the phone attribute to contain a string of integers' do
+    notification = FactoryGirl.build_stubbed(:notification)
+    notification.phone = "atextphonenumber"
+    expect(notification).to_not be_valid
+  end
+
+  xit 'requires the phone attribute to only have 10 characters' do
+    @notification.phone = "12345678901"
+    expect(@notification).to_not be_valid
+  end
+
+  xit 'limits the body attribute to 160 characters' do
+    @notification.body = "word" * 500
+    expect(@notification).to_not be_valid
+  end
+
 end
