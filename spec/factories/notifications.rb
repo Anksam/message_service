7d96c66eec2170d06b669930a44983e8c3120b0c
Ankesh <aankeshsamarth@gmail.com>
2017-03-03 01:28:20 +0530

    Refactored

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,13 +1,19 @@
 FactoryGirl.define do
   factory :notification do
-    phone "1111111111"
+    phone "5555555555"
     body "My message"
     source_app "some_app"
   end
 
   factory :second_notification, class: 'Notification' do
-    phone "2222222222"
-    body "My message"
+    phone "4444444444"
+    body "Another notification"
     source_app "some_app"
+  end
+
+  factory :outside_notification, class: 'Notification' do
+    phone "3333333333"
+    body "Third notification"
+    source_app "different_app"
   end
 end
